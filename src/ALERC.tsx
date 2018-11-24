de0376300cdae672b9100371075e7ac6d3777ebc
ontofractal <ontofractal@protonmail.com>
2018-11-24 18:03:55 +0200

    add Authors Link Equity Regeneration Campaign page

---
+++ src/ALERC.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const ALERC = () => {
+    return <div className={"ALERC"}>
+        <h1>
+            Authors Link Equity Regeneration Campaign
+        </h1>
+        <p>
+            <ul>
+                <li>
+                    Educate authors on importance of link equity and ways to generate intrinsic and extrinsic value from
+                    it
+                </li>
+                <li>
+                    Create a step by step checklist for creators on scrappy and sustainable way to host their own
+                    domains and canonical URLs
+                </li>
+                <li>
+                    Analyze and quantify link graphs data using Common Crawl project
+                </li>
+                <li>
+                    Build a provisional registry of canonical URLs
+                </li>
+                <li>
+                    Work with Wikipedia to replace links to hosting platforms with links to authors' domains
+                </li>
+                <li>
+                    Build tools to scan the internet for links pointing to hosting platforms and automagically replace
+                    attribution to authors' websites, for example, using a Wordpress plugin
+                </li>
+                <li>
+                    Build social media bots that attempt to detect Creative Commons images and automatically post
+                    attribution data
+                </li>
+            </ul>
+        </p>
+    </div>;
+};
+
+export default ALERC;
--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, {Component} from "react";
 import "./App.css";
 import Frontpage from "./frontpage/Frontpage";
+import ALERC from "./ALERC"
 import {BrowserRouter} from "react-router-dom";
 import {Route, Switch} from "react-router";
 
@@ -21,6 +22,7 @@ class App extends Component {
                     <main>
                         <Switch>
                             <Route exact path="/" component={Frontpage}/>
+                            <Route exact path="/alerc" component={ALERC}/>
                             <Route exact path="/registry" component={Registry}/>
                             <Route exact path="/attribution" component={Attribution}/>
                         </Switch>
