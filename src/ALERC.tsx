edf048eb18af439bb39b5e3e98d970bc4e501f60
ontofractal <ontofractal@protonmail.com>
2018-11-29 19:47:46 +0200

    add rel=canonical description to the ALERC page

--- src/ALERC.tsx
+++ src/ALERC.tsx
@@ -34,6 +34,37 @@ const ALERC = () => {
                 </li>
             </ul>
         </p>
+        <h1>
+            Link equity transfer using rel="canonical"
+        </h1>
+        <p>
+            There is a standard <a href={"https://tools.ietf.org/html/rfc6596"}>RFC6596</a>, cheap and technically
+            simple
+            way for any website to redirect link equity by using a canonical link relation.
+
+            Rel="canonical" is supported by Google, Yahoo and Bing.
+
+            <h3>
+                Further reading
+            </h3>
+            <ul>
+                <li>
+                    <a href="https://support.google.com/webmasters/answer/139066?hl=en"> Google</a>
+                </li>
+                <li>
+                    <a href="https://moz.com/blog/rel-canonical"> Moz.com</a>
+                </li>
+                <li>
+                    <a href={"https://en.wikipedia.org/wiki/Canonical_link_element"}>Canonical link element on Wikipedia
+                    </a>
+                </li>
+            </ul>
+
+            Implementation of rel="canonical" link tag by platforms for Creative Commons works would immediately restore an enormous
+            amount of value for CC-licensed authors. Unfortunately, platforms and authors incentives are not aligned and it
+            would be naive to assume that platforms would release captured link equity.
+
+        </p>
     </div>;
 };
 
